2bb612161b577aa28796e9deb31de38df7420741
Schwald Kevin <kevin.schwald@students.bfh.ch>
2023-08-26 00:15:29 +0200

    addes MockData

---
+++ backend/src/mockData.ts
@@ -0,0 +1,291 @@
+import {Municipality} from "./interfaces";
+
+export const mochData: Municipality = {
+    meta: {
+        id: 1,
+        municipalityName: 'Paradise',
+        surveyParticipants: 12
+    },
+    facts: {
+        environment: {
+            biodiversity: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            climate: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            energy: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            air: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            water: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            soil: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            environmentalConsumption: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+        },
+        social: {
+            supplyAndServices: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            health: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            qualityOfLiving: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            security: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            socialSupportAndSolidarity: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            equalityAndIntegration: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            leisureTimeOffers: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            mobility: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            education: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+
+        },
+        economy: {
+            wageAndCostOfLiving: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            occupation: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            professionals: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            economicalResiliance: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            innovation: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            attractivenessForEmployers: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+        }
+    },
+    survey: {
+        environment: {
+            biodiversity: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            climate: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            energy: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            air: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            water: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            soil: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            environmentalConsumption: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+        },
+        social: {
+            supplyAndServices: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            health: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            qualityOfLiving: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            security: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            socialSupportAndSolidarity: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            equalityAndIntegration: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            leisureTimeOffers: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            mobility: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            education: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+
+        },
+        economy: {
+            wageAndCostOfLiving: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            occupation: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            professionals: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            economicalResiliance: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            innovation: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ],
+            attractivenessForEmployers: [
+                {
+                    name: 'test',
+                    value: 12
+                }
+            ]
+        }
+    }
+}
--- frontend/src/data/interfaces.ts
+++ frontend/src/data/interfaces.ts
@@ -1,5 +1,6 @@
 export interface Municipality {
     meta: {
+      id: number;
         municipalityName: string;
         surveyParticipants: number;
     },
