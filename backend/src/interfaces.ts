9c425c927cdbdadc7e13ca5ec9fb928f0e05a866
Gujer Nicolas Joël <nicolas.gujer@bfh.ch>
2023-08-26 09:50:13 +0200

    prettier

--- backend/src/interfaces.ts
+++ backend/src/interfaces.ts
@@ -1,46 +1,46 @@
 export interface Municipality {
-    meta: {
-        id: number;
-        municipalityName: string;
-        surveyParticipants: number;
-    },
-    facts: SectorData;
-    survey: SectorData;
+  meta: {
+    id: number;
+    municipalityName: string;
+    surveyParticipants: number;
+  };
+  facts: SectorData;
+  survey: SectorData;
 }
 
 export interface SectorData {
-    environment: {
-        biodiversity: Indicator[];
-        climate: Indicator[];
-        energy: Indicator[];
-        air: Indicator[];
-        water: Indicator[];
-        soil: Indicator[];
-        environmentalConsumption: Indicator[];
-    },
-    social: {
-        supplyAndServices: Indicator[];
-        health: Indicator[];
-        qualityOfLiving: Indicator[]; // Wohnangebot und Raumentwicklung
-        security: Indicator[];
-        socialSupportAndSolidarity: Indicator[];
-        equalityAndIntegration: Indicator[];
-        leisureTimeOffers: Indicator[];
-        mobility: Indicator[];
-        education: Indicator[];
-    },
-    economy: {
-        wageAndCostOfLiving: Indicator[];
-        occupation: Indicator[];
-        professionals: Indicator[];
-        economicalResiliance: Indicator[];
-        innovation: Indicator[];
-        attractivenessForEmployers: Indicator[];
-    }
+  environment: {
+    biodiversity: Indicator[];
+    climate: Indicator[];
+    energy: Indicator[];
+    air: Indicator[];
+    water: Indicator[];
+    soil: Indicator[];
+    environmentalConsumption: Indicator[];
+  };
+  social: {
+    supplyAndServices: Indicator[];
+    health: Indicator[];
+    qualityOfLiving: Indicator[]; // Wohnangebot und Raumentwicklung
+    security: Indicator[];
+    socialSupportAndSolidarity: Indicator[];
+    equalityAndIntegration: Indicator[];
+    leisureTimeOffers: Indicator[];
+    mobility: Indicator[];
+    education: Indicator[];
+  };
+  economy: {
+    wageAndCostOfLiving: Indicator[];
+    occupation: Indicator[];
+    professionals: Indicator[];
+    economicalResiliance: Indicator[];
+    innovation: Indicator[];
+    attractivenessForEmployers: Indicator[];
+  };
 }
 
 export interface Indicator {
-    name: string;
-    value: number | undefined;
-    participants?: number;
+  name: string;
+  value: number | undefined;
+  participants?: number;
 }
--- backend/src/mockData.ts
+++ backend/src/mockData.ts
@@ -1,297 +1,295 @@
-import {Municipality} from "./interfaces";
+import { Municipality } from './interfaces';
 
 export function mockData(): Municipality {
-    return {
-        meta: {
-            id: 1,
-            municipalityName: 'Paradise',
-            surveyParticipants: randomNumber()
-        },
-        facts: {
-            environment: {
-                biodiversity: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                climate: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                energy: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                air: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                water: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                soil: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                environmentalConsumption: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-            },
-            social: {
-                supplyAndServices: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                health: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                qualityOfLiving: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                security: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                socialSupportAndSolidarity: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                equalityAndIntegration: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                leisureTimeOffers: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                mobility: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                education: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-
-            },
-            economy: {
-                wageAndCostOfLiving: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                occupation: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                professionals: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                economicalResiliance: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                innovation: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                attractivenessForEmployers: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-            }
-        },
-        survey: {
-            environment: {
-                biodiversity: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                climate: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                energy: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                air: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                water: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                soil: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                environmentalConsumption: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-            },
-            social: {
-                supplyAndServices: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                health: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                qualityOfLiving: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                security: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                socialSupportAndSolidarity: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                equalityAndIntegration: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                leisureTimeOffers: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                mobility: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                education: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-
-            },
-            economy: {
-                wageAndCostOfLiving: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                occupation: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                professionals: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                economicalResiliance: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                innovation: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ],
-                attractivenessForEmployers: [
-                    {
-                        name: 'test',
-                        value: randomNumber()
-                    }
-                ]
-            }
-        }
-    }
+  return {
+    meta: {
+      id: 1,
+      municipalityName: 'Paradise',
+      surveyParticipants: randomNumber(),
+    },
+    facts: {
+      environment: {
+        biodiversity: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        climate: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        energy: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        air: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        water: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        soil: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        environmentalConsumption: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+      social: {
+        supplyAndServices: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        health: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        qualityOfLiving: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        security: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        socialSupportAndSolidarity: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        equalityAndIntegration: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        leisureTimeOffers: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        mobility: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        education: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+      economy: {
+        wageAndCostOfLiving: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        occupation: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        professionals: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        economicalResiliance: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        innovation: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        attractivenessForEmployers: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+    },
+    survey: {
+      environment: {
+        biodiversity: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        climate: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        energy: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        air: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        water: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        soil: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        environmentalConsumption: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+      social: {
+        supplyAndServices: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        health: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        qualityOfLiving: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        security: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        socialSupportAndSolidarity: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        equalityAndIntegration: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        leisureTimeOffers: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        mobility: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        education: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+      economy: {
+        wageAndCostOfLiving: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        occupation: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        professionals: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        economicalResiliance: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        innovation: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+        attractivenessForEmployers: [
+          {
+            name: 'test',
+            value: randomNumber(),
+          },
+        ],
+      },
+    },
+  };
 }
 
 export function randomNumber() {
-    return Math.floor(Math.random() * 11)
+  return Math.floor(Math.random() * 11);
 }
